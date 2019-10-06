e95ff8a08c5f633ee9a8fd02a05850390191236a
MrXY <38537813+DwiWardana@users.noreply.github.com>
2019-10-06 21:13:23 +0800

    Update pxploit.py

--- pxploit.py
+++ pxploit.py
@@ -1,11 +1,12 @@
 #!/bin/env python3
 import sys
-import time
+import datetime
 
 try:
     import requests
     import socket
     import pandas
+    import user_agent
 except Exception as e:
     e = str(e).split("'")[1]
     print("No module named {a}\nTry run this syntax: pip install {a}".format(a=e))
@@ -15,18 +16,24 @@ def scanner(i,p):
     for x in p:
         try:
             s = socket.socket()
-            s.settimeout(0.7)
+            s.settimeout(0.1)
             s.connect((i, int(x)))
-            s.settimeout(3)
+            s.settimeout(5)
             service = ""
             try:
                 service = s.recv(1024).decode()
             except Exception as e:
                 e = str(e)
-                if "utf-8" in e:
-                    service = s.recv(1024)
-                else:
-                    service = "Can't find service"
+                data = open('headers.txt', 'r').read()
+                packet = str("GET / HTTP/1.1\nHost: "+i+"\n\n User-Agent: "+user_agent.generate_user_agent()+"\n"+data).encode('utf-8')
+                try:
+                    s.sendto(packet, (i,x))
+                    if "utf-8" in e:
+                        service = s.recv(1024)
+                    else:
+                        service = s.recv(1024).decode()
+                except Exception as e:
+                    service = "Can't find service (Timeout)"
             print("\r┏━Port {} Open".format(x))
             print("┗━🢖 {}".format(service))
         except socket.error as e:
@@ -59,6 +66,7 @@ By: MrXY ( mrxy@parsect.com )
 PARSECT PXploit\n""")
         host = socket.gethostbyname(str(input("(ip/domain): ")))
         port = [x for x in range(1,65535)]
+        print("Starting PXploit at {}\n".format(datetime.datetime.now()))
         scanner(host,port)
     except KeyboardInterrupt:
         print("\n[!] Exit (Ctrl+C)")
